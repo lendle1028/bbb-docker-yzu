25110409e1d60cfc8b1949a53c17c5af33d3bf41
lendle <lendle_tseng@seed.net.tw>
2020-12-02 15:38:40 +0800

    v1.1 need only bbb-conf --restart

--- greenlight_yzu/nginx/setup_bbb.sh
+++ greenlight_yzu/nginx/setup_bbb.sh
@@ -1,6 +1,7 @@
 rm /etc/nginx/sites-enabled/default.old 
 openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /etc/nginx/additional_conf/ssl/nginx.key -out /etc/nginx/additional_conf/ssl/nginx.crt
 openssl x509 -inform PEM -in /etc/nginx/additional_conf/ssl/nginx.crt > /etc/nginx/additional_conf/ssl/bbb.pem
+bbb-conf --setip bbb.imsofa.rocks
 ln -sf /etc/nginx/additional_conf/bigbluebutton.properties /usr/share/bbb-web/WEB-INF/classes/
 ln -sf /etc/nginx/additional_conf/screenshare.properties /usr/share/red5/webapps/screenshare/WEB-INF/
 ln -sf /etc/nginx/additional_conf/config.xml /var/www/bigbluebutton/client/conf/
@@ -11,5 +12,4 @@ ln -sf /etc/nginx/additional_conf/src/bbb/usr/share/bbb-web/WEB-INF/classes/ /us
 ln -sf /etc/nginx/additional_conf/src/bbb/bbb-html5.nginx /etc/bigbluebutton/nginx/
 rm -R /var/www/bigbluebutton-default/
 ln -sf /etc/nginx/additional_conf/src/bbb/bigbluebutton-default/ /var/www/
-bbb-conf --setip bbb.imsofa.rocks
 bbb-conf --restart
--- greenlight_yzu/nginx/setup_greenlight.sh
+++ greenlight_yzu/nginx/setup_greenlight.sh
@@ -1,4 +1,6 @@
 cp /etc/nginx/additional_conf/ssl/bbb.pem /usr/local/share/ca-certificates/CA.crt 
 update-ca-certificates 
-ln -sf /etc/nginx/additional_conf/src/greenlight/app /usr/src/app/app/
-ln -sf /etc/nginx/additional_conf/src/greenlight/config /usr/src/app/config/
+rm -R /usr/src/app/app
+rm -R /usr/src/app/config
+ln -sf /etc/nginx/additional_conf/src/greenlight/app /usr/src/app/
+ln -sf /etc/nginx/additional_conf/src/greenlight/config /usr/src/app/
